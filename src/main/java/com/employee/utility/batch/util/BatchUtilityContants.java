92f5d944674ad55e7eb22703e477e58197e7ea43
msatheeshkumar <msatheeshkumar@users.noreply.github.com>
2015-06-03 10:43:57 +0530

    Update BatchUtilityContants.java
    removed unwanted comment

--- src/main/java/com/employee/utility/batch/util/BatchUtilityContants.java
+++ src/main/java/com/employee/utility/batch/util/BatchUtilityContants.java
@@ -1,12 +1,3 @@
-/*
- * This code contains copyright information which is the proprietary property
- * of SITA Information Network Computing Limited (SITA). No part of this
- * code may be reproduced, stored or transmitted in any form without the prior
- * written permission of SITA.
- *
- * Copyright (C) SITA Information Network Computing Limited 2011-2012.
- * All rights reserved.
- */
 package com.employee.utility.batch.util;
 
 /**
